b745e9a6e7bf291308dd1c0c7a2a130214e99fb0
radyuk.aleksandr2 <radyuk.aleksandr2@rwb.ru>
2025-07-14 05:16:07 +0300

    sprint 8, addressing comments

--- parcel.go
+++ parcel.go
@@ -60,6 +60,10 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 		res = append(res, p)
 	}
 
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
 	return res, nil
 }
 
--- parcel_test.go
+++ parcel_test.go
@@ -42,15 +42,13 @@ func TestAddGetDelete(t *testing.T) {
 	id, err := store.Add(parcel)
 	require.NoError(t, err)
 	require.NotZero(t, id)
+
 	parcel.Number = id
 
 	// get
 	storedParcel, err := store.Get(id)
 	require.NoError(t, err)
-	require.Equal(t, parcel.Client, storedParcel.Client)
-	require.Equal(t, parcel.Status, storedParcel.Status)
-	require.Equal(t, parcel.Address, storedParcel.Address)
-	require.Equal(t, parcel.CreatedAt, storedParcel.CreatedAt)
+	require.Equal(t, parcel, storedParcel)
 
 	// delete
 	err = store.Delete(id)
@@ -159,9 +157,6 @@ func TestGetByClient(t *testing.T) {
 		// убедитесь, что значения полей полученных посылок заполнены верно
 		originalParcel, ok := parcelMap[parcel.Number]
 		require.True(t, ok)
-		require.Equal(t, originalParcel.Client, parcel.Client)
-		require.Equal(t, originalParcel.Status, parcel.Status)
-		require.Equal(t, originalParcel.Address, parcel.Address)
-		require.Equal(t, originalParcel.CreatedAt, parcel.CreatedAt)
+		require.Equal(t, originalParcel, parcel)
 	}
 }
